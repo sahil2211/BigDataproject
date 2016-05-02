171bcb5f8ef55b89909ffd1d31808e04b2c8d8f8
tailorfenil <tailorfenil@gmail.com>
2016-05-01 20:48:17 -0400

    Per_Hour_Analysis(Includes mapper,reducer,.Rmd and JPG file

---
+++ Per_hour_analysis/per_hour_analysis_reduce.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+import sys
+
+current_key = None
+current_sum = 0
+
+for line in sys.stdin:
+
+    key, count = line.strip().split("\t", 1)
+
+    try:
+        count = int(count)
+    except ValueError:
+        continue
+
+    if key == current_key:
+        current_sum += count
+    else:
+        if current_key:
+            print ("%s,%d" % (current_key, current_sum))
+        current_key = key
+        current_sum = count
+
+print ("%s,%d" % (current_key, current_sum))
