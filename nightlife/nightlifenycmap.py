8e3563a4e20e2229e4b079f838de01eb1f55cbc7
tailorfenil <tailorfenil@gmail.com>
2016-05-03 19:31:00 -0400

    nightlife_map_modified_to_get_only_weekend_values

--- nightlife/nightlifenycmap.py
+++ nightlife/nightlifenycmap.py
@@ -9,6 +9,7 @@ import numpy, shapefile, time
 from datetime import datetime
 flag = None
 lookup = {'216': ['Queens'], '217': ['Brooklyn'], '214': ['Staten Island'], '215': ['Queens'], '212': ['Bronx'], '213': ['Bronx'], '210': ['Brooklyn'], '211': ['Manhattan'], '165': ['Brooklyn'], '264': ['Unknown'], '265': ['Unknown'], '218': ['Queens'], '219': ['Queens'], '133': ['Brooklyn'], '132': ['Queens'], '131': ['Queens'], '130': ['Queens'], '137': ['Manhattan'], '136': ['Bronx'], '135': ['Queens'], '134': ['Queens'], '139': ['Queens'], '138': ['Queens'], '166': ['Manhattan'], '24': ['Manhattan'], '25': ['Brooklyn'], '26': ['Brooklyn'], '27': ['Queens'], '20': ['Bronx'], '21': ['Brooklyn'], '22': ['Brooklyn'], '23': ['Staten Island'], '160': ['Queens'], '28': ['Queens'], '29': ['Brooklyn'], '161': ['Manhattan'], '4': ['Manhattan'], '8': ['Queens'], '163': ['Manhattan'], '119': ['Bronx'], '258': ['Queens'], '120': ['Manhattan'], '121': ['Queens'], '122': ['Queens'], '123': ['Brooklyn'], '124': ['Queens'], '125': ['Manhattan'], '126': ['Bronx'], '127': ['Manhattan'], '128': ['Manhattan'], '129': ['Queens'], '167': ['Bronx'], '118': ['Staten Island'], '59': ['Bronx'], '58': ['Bronx'], '55': ['Brooklyn'], '54': ['Brooklyn'], '57': ['Queens'], '56': ['Queens'], '51': ['Bronx'], '50': ['Manhattan'], '53': ['Queens'], '52': ['Brooklyn'], '259': ['Bronx'], '164': ['Manhattan'], '201': ['Queens'], '199': ['Bronx'], '179': ['Queens'], '200': ['Bronx'], '195': ['Brooklyn'], '194': ['Manhattan'], '197': ['Queens'], '178': ['Brooklyn'], '191': ['Queens'], '190': ['Brooklyn'], '193': ['Queens'], '192': ['Queens'], '115': ['Staten Island'], '114': ['Manhattan'], '88': ['Manhattan'], '89': ['Brooklyn'], '111': ['Brooklyn'], '110': ['Staten Island'], '113': ['Manhattan'], '112': ['Brooklyn'], '82': ['Queens'], '83': ['Queens'], '80': ['Brooklyn'], '81': ['Bronx'], '86': ['Queens'], '87': ['Manhattan'], '84': ['Staten Island'], '85': ['Brooklyn'], '251': ['Staten Island'], '198': ['Queens'], '256': ['Brooklyn'], '206': ['Staten Island'], '226': ['Queens'], '257': ['Brooklyn'], '3': ['Bronx'], '177': ['Brooklyn'], '254': ['Bronx'], '7': ['Queens'], '247': ['Bronx'], '255': ['Brooklyn'], '225': ['Brooklyn'], '245': ['Staten Island'], '244': ['Manhattan'], '108': ['Brooklyn'], '109': ['Staten Island'], '241': ['Bronx'], '240': ['Bronx'], '243': ['Manhattan'], '242': ['Bronx'], '102': ['Queens'], '103': ['Manhattan'], '100': ['Manhattan'], '101': ['Queens'], '106': ['Brooklyn'], '107': ['Manhattan'], '104': ['Manhattan'], '105': ['Manhattan'], '39': ['Brooklyn'], '38': ['Queens'], '33': ['Brooklyn'], '32': ['Bronx'], '31': ['Bronx'], '30': ['Queens'], '37': ['Brooklyn'], '36': ['Brooklyn'], '35': ['Brooklyn'], '34': ['Brooklyn'], '246': ['Manhattan'], '73': ['Queens'], '252': ['Queens'], '205': ['Queens'], '223': ['Queens'], '176': ['Staten Island'], '60': ['Bronx'], '61': ['Brooklyn'], '62': ['Brooklyn'], '63': ['Brooklyn'], '64': ['Queens'], '65': ['Brooklyn'], '66': ['Brooklyn'], '67': ['Brooklyn'], '68': ['Manhattan'], '69': ['Bronx'], '175': ['Queens'], '174': ['Bronx'], '173': ['Queens'], '172': ['Staten Island'], '171': ['Queens'], '170': ['Manhattan'], '203': ['Queens'], '222': ['Brooklyn'], '181': ['Brooklyn'], '253': ['Queens'], '248': ['Bronx'], '182': ['Bronx'], '183': ['Bronx'], '180': ['Queens'], '2': ['Queens'], '162': ['Manhattan'], '187': ['Staten Island'], '184': ['Bronx'], '6': ['Staten Island'], '220': ['Bronx'], '186': ['Manhattan'], '188': ['Brooklyn'], '189': ['Brooklyn'], '202': ['Manhattan'], '196': ['Queens'], '221': ['Staten Island'], '185': ['Bronx'], '99': ['Staten Island'], '98': ['Queens'], '168': ['Bronx'], '169': ['Bronx'], '229': ['Manhattan'], '228': ['Brooklyn'], '91': ['Brooklyn'], '90': ['Manhattan'], '93': ['Queens'], '92': ['Queens'], '95': ['Queens'], '94': ['Bronx'], '97': ['Brooklyn'], '96': ['Queens'], '11': ['Brooklyn'], '10': ['Queens'], '13': ['Manhattan'], '12': ['Manhattan'], '15': ['Queens'], '14': ['Brooklyn'], '17': ['Brooklyn'], '16': ['Queens'], '19': ['Queens'], '18': ['Bronx'], '117': ['Queens'], '250': ['Bronx'], '116': ['Manhattan'], '204': ['Staten Island'], '151': ['Manhattan'], '150': ['Brooklyn'], '153': ['Manhattan'], '152': ['Manhattan'], '155': ['Brooklyn'], '154': ['Brooklyn'], '157': ['Queens'], '156': ['Staten Island'], '159': ['Bronx'], '158': ['Manhattan'], '234': ['Manhattan'], '238': ['Manhattan'], '239': ['Manhattan'], '207': ['Queens'], '235': ['Bronx'], '236': ['Manhattan'], '237': ['Manhattan'], '230': ['Manhattan'], '231': ['Manhattan'], '232': ['Manhattan'], '233': ['Manhattan'], '224': ['Manhattan'], '48': ['Manhattan'], '49': ['Brooklyn'], '46': ['Bronx'], '47': ['Bronx'], '44': ['Staten Island'], '45': ['Manhattan'], '42': ['Manhattan'], '43': ['Manhattan'], '40': ['Brooklyn'], '41': ['Manhattan'], '1': ['EWR'], '5': ['Staten Island'], '9': ['Queens'], '146': ['Queens'], '147': ['Bronx'], '144': ['Manhattan'], '145': ['Queens'], '142': ['Manhattan'], '143': ['Manhattan'], '140': ['Manhattan'], '141': ['Manhattan'], '209': ['Manhattan'], '208': ['Bronx'], '148': ['Manhattan'], '149': ['Brooklyn'], '77': ['Brooklyn'], '76': ['Brooklyn'], '75': ['Manhattan'], '74': ['Manhattan'], 'LocationID': ['Borough'], '72': ['Brooklyn'], '71': ['Brooklyn'], '70': ['Queens'], '79': ['Manhattan'], '78': ['Bronx'], '263': ['Manhattan'], '249': ['Manhattan'], '262': ['Manhattan'], '227': ['Brooklyn'], '261': ['Manhattan'], '260': ['Queens']}
+weekend=set([4,5,6])
 
 def findNeighborhood(location, index, neighborhoods):
     match = index.intersection((location[0], location[1], location[0], location[1]))
@@ -45,41 +46,50 @@ for line in sys.stdin:
     dropoff_neighborhood=-1        # default as first
     total = -1              # default as first
     a = values[0]
-    if len(values) == 19:# yellowtaxi_data
+    if len(values) == 19 or len(values)==18:# yellowtaxi_data
         dropoff_location = (float(values[9]), float(values[10]))
         dropoff_neighborhood = findNeighborhood(dropoff_location, index, neighborhoods)
         dropoff_datetime = values[2]
-        dropoff_hour = int(datetime.strptime(dropoff_datetime, '%Y-%m-%d %H:%M:%S').strftime('%H'))
+        dropoff_year=((datetime.strptime(dropoff_datetime, '%Y-%m-%d %H:%M:%S').strftime('%Y')))
         dropoff_month = int(datetime.strptime(dropoff_datetime, '%Y-%m-%d %H:%M:%S').strftime('%m'))
-        if((dropoff_hour>=21 and dropoff_hour<=23) or (dropoff_hour>=0 and dropoff_hour<=4)):
+        dropoff_date = int(datetime.strptime(dropoff_datetime, '%Y-%m-%d %H:%M:%S').strftime('%d'))
+        dateset=datetime(int(dropoff_year),int(dropoff_month),int(dropoff_date))
+        dropoff_hour = int(datetime.strptime(dropoff_datetime, '%Y-%m-%d %H:%M:%S').strftime('%H'))
+
+        if(((dropoff_hour>=21 and dropoff_hour<=23) or (dropoff_hour>=0 and dropoff_hour<=2)) and (dateset.weekday() in weekend)):
             if "Manhattan" in neighborhoods[dropoff_neighborhood][0]:
-                print ("%s,Manhattan,%.3f,%.3f,y\t%d" % (dropoff_month,float(values[9]),float(values[10]),1))
+                print ("%s,Manhattan,%.3f,%.3f,y\t%d" % (dropoff_hour,float(values[9]),float(values[10]),1))
             if "Bronx" in neighborhoods[dropoff_neighborhood][0]:
-                print ("%s,Bronx,%.3f,%.3f,y\t%d" % (dropoff_month,float(values[9]),float(values[10]),1))
+                print ("%s,Bronx,%.3f,%.3f,y\t%d" % (dropoff_hour,float(values[9]),float(values[10]),1))
             if "Brooklyn" in neighborhoods[dropoff_neighborhood][0]:
-                print ("%s,Brooklyn,%.3f,%.3f,y\t%d" % (dropoff_month,float(values[9]),float(values[10]),1))
+                print ("%s,Brooklyn,%.3f,%.3f,y\t%d" % (dropoff_hour,float(values[9]),float(values[10]),1))
             if "Queens" in neighborhoods[dropoff_neighborhood][0]:
-                print ("%s,Queens,%.3f,%.3f,y\t%d" % (dropoff_month,float(values[9]),float(values[10]),1))
+                print ("%s,Queens,%.3f,%.3f,y\t%d" % (dropoff_hour,float(values[9]),float(values[10]),1))
             if "Staten Island" in neighborhoods[dropoff_neighborhood][0]:
-                print ("%s,Staten Island,%.3f,%.3f,y\t%d" % (dropoff_month,float(values[9]),float(values[10]),1))
+                print ("%s,Staten Island,%.3f,%.3f,y\t%d" % (dropoff_hour,float(values[9]),float(values[10]),1))
 
 
-    elif len(values) == 23:
+    elif len(values) == 23: #green taxi
         dropoff_location = (float(values[7]), float(values[8]))
         dropoff_neighborhood = findNeighborhood(dropoff_location, index, neighborhoods)
         dropoff_datetime = values[2]
-        dropoff_hour = int(datetime.strptime(dropoff_datetime, '%Y-%m-%d %H:%M:%S').strftime('%H'))
+        dropoff_year=((datetime.strptime(dropoff_datetime, '%Y-%m-%d %H:%M:%S').strftime('%Y')))
         dropoff_month = int(datetime.strptime(dropoff_datetime, '%Y-%m-%d %H:%M:%S').strftime('%m'))
-        if((dropoff_hour>=21 and dropoff_hour<=23) or (dropoff_hour>=0 and dropoff_hour<=4)):
+        dropoff_date = int(datetime.strptime(dropoff_datetime, '%Y-%m-%d %H:%M:%S').strftime('%d'))
+        dateset=datetime(int(dropoff_year),int(dropoff_month),int(dropoff_date))
+        #if(dateset.weekday() in weekend):
+        #        print(dateset.weekday())
+        dropoff_hour = int(datetime.strptime(dropoff_datetime, '%Y-%m-%d %H:%M:%S').strftime('%H'))
+        if((dropoff_hour>=21 and dropoff_hour<=23) or (dropoff_hour>=0 and dropoff_hour<=2) and (dateset.weekday() in weekend)):
             if "Manhattan" in neighborhoods[dropoff_neighborhood][0]:
-                print ("%s,Manhattan,%.3f,%.3f,g\t%d" % (dropoff_month,float(values[7]),float(values[8]),1))
+                print ("%s,Manhattan,%.3f,%.3f,g\t%d" % (dropoff_hour,float(values[7]),float(values[8]),1))
             if "Bronx" in neighborhoods[dropoff_neighborhood][0]:
                 print ("%s,Bronx,%.3f,%.3f,g\t%d" % (dropoff_month,float(values[7]),float(values[8]),1))
             if "Brooklyn" in neighborhoods[dropoff_neighborhood][0]:
-                print ("%s,Brooklyn,%.3f,%.3f,g\t%d" % (dropoff_month,float(values[7]),float(values[8]),1))
+                print ("%s,Brooklyn,%.3f,%.3f,g\t%d" % (dropoff_hour,float(values[7]),float(values[8]),1))
             if "Queens" in neighborhoods[dropoff_neighborhood][0]:
-                print ("%s,Queens,%.3f,%.3f,g\t%d" % (dropoff_month,float(values[7]),float(values[8]),1))
+                print ("%s,Queens,%.3f,%.3f,g\t%d" % (dropoff_hour,float(values[7]),float(values[8]),1))
             if "Staten Island" in neighborhoods[dropoff_neighborhood][0]:
-                print ("%s,Staten Island,%.3f,%.3f,g\t%d" % (dropoff_month,float(values[7]),float(values[8]),1))
+                print ("%s,Staten Island,%.3f,%.3f,g\t%d" % (dropoff_hour,float(values[7]),float(values[8]),1))
 
 
