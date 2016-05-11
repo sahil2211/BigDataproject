e4b9b6dc0ecc716889e57d8c795453aa09aa8482
sahil shah <sahil shah>
2016-05-11 19:37:14 -0400

    added bootstrap shell file to install python modules in aws cluster

---
+++ rtree.sh
@@ -0,0 +1,5 @@
+sudo yum-config-manager --enable epel
+sudo yum install -y spatialindex spatialindex-devel
+sudo ln -sf /usr/bin/python2.7 /usr/bin/python
+sudo pip install Rtree==0.7.0
+sudo pip install pyshp
